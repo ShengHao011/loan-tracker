220efacf1acf48014988e7f97eaa789d467b60e2
fred <sheng726@students.cs.ubc.ca>
2022-10-15 22:07:23 -0700

    Added ListOfLoan class test

---
+++ src/main/model/ListOfLoanTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ListOfLoanTest {
+
+    @Test
+    void constructorTest() {
+        ListOfLoan testList = new ListOfLoan();
+        assertEquals(0, testList.getListOfLoan().size());
+    }
+}
